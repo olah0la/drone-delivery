d12f8001045bf43728b22040a1d7b06d15453191
Steven <steven.ola@icm-institute.org>
2025-07-08 22:54:08 +0200

    1. Naming issues. clarifying and specifying functionality 2.using the `from_orm` method for pydantic validation. 3. removing the uneccesary empty query validation.

--- event-collector/app/services/delivery_service.py
+++ event-collector/app/services/delivery_service.py
@@ -7,7 +7,7 @@ from ..models import Delivery, DeliveryState
 from ..api.crud import (
     count_deliveries_by_state,
     count_total_deliveries,
-    read_delivery,
+    read_delivery_by_name,
     update_delivery,
     handle_new_delivery,
     get_deliveries_by_state
@@ -16,8 +16,11 @@ from ..api.crud import (
 
 ONGOING_STATES = (DeliveryState.TAKEN_OFF, DeliveryState.PARCEL_COLLECTED, DeliveryState.LANDED)
 
-async def count_deliveries(db: AsyncSession) -> DeliveryCountSchema:
-    """Count the total number of ongoing and total deliveries."""
+async def get_delivery_counts(db: AsyncSession) -> DeliveryCountSchema:
+    """
+    Count ongoing and total deliveries.
+    Returns a DeliveryCountSchema with the counts of ongoing and total deliveries."""
+    """"""
     ongoing_deliveries = await count_deliveries_by_state(db, ONGOING_STATES)
     total_deliveries = await count_total_deliveries(db)
     return DeliveryCountSchema(
@@ -26,16 +29,21 @@ async def count_deliveries(db: AsyncSession) -> DeliveryCountSchema:
     )
 
 async def get_ongoing_deliveries(db: AsyncSession) -> List[DeliverySchema]:
-    """Retrieve all ongoing deliveries."""
+    """
+    Retrieve all ongoing deliveries.
+    Returns a list of DeliverySchema for ongoing deliveries.
+    """
     deliveries = await get_deliveries_by_state(db, ONGOING_STATES)
-    if not deliveries:
-        return []
-    return [delivery.to_dict() for delivery in deliveries] 
+    return [DeliverySchema.from_orm(delivery) for delivery in deliveries]
 
 async def handle_delivery_event(db: AsyncSession, event_type: DeliveryState, delivery_name: str) -> Delivery:
-    """Handle a delivery event by updating the delivery status."""
-    # FU: fix the id =/= name issue
-    delivery = await read_delivery(db, delivery_name)
+    """
+    Handle a delivery event by either creating a new delivery or updating an existing one.
+    If the delivery does not exist, it will be created with the provided name and event type
+    If it exists, its status will be updated to the provided event type.
+    Returns the Delivery object.
+    """
+    delivery = await read_delivery_by_name(db, delivery_name)
     if not delivery:
         delivery = await handle_new_delivery(db, delivery_name, event_type)
     else:
